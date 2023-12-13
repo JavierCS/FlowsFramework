30101553b5a61d4913b3df804643fc299c1b871d
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2023-12-13 01:53:00 -0600

    [NEW] - Se agrega FFController en la carpeta TestGroup para pruebas de integración y visivilidad entre el framework y la app

---
+++ FlowsFramework/TestGroup/FFController.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+public class FFController: UIViewController {
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.orange
+    }
+}
