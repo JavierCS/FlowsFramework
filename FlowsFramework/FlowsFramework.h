9fd42325236ef862315e4cc66ffe9fc0afbf7e37
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2023-12-12 22:21:48 -0600

    Initial Commit

---
+++ FlowsFramework/FlowsFramework.h
@@ -0,0 +1,18 @@
+//
+//  FlowsFramework.h
+//  FlowsFramework
+//
+//  Created by Javier Cruz Santiago on 12/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlowsFramework.
+FOUNDATION_EXPORT double FlowsFrameworkVersionNumber;
+
+//! Project version string for FlowsFramework.
+FOUNDATION_EXPORT const unsigned char FlowsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowsFramework/PublicHeader.h>
+
+
